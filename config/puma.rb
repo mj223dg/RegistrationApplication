cbb6f022ac84408df552d00dc68736f823f937d0
mj223dg <mj223dg@student.lnu.se>
2016-04-12 15:14:41 +0200

    heroku fix

--- config/puma.rb
+++ config/puma.rb
@@ -9,14 +9,13 @@ class Puma
   port        ENV['PORT']     || 3000
   environment ENV['RACK_ENV'] || 'development'
 
-    on_worker_boot do
-      # Valid on Rails up to 4.1 the initializer method of setting `pool` size
-      ActiveSupport.on_load(:active_record) do
-        config = ActiveRecord::Base.configurations[Rails.env] ||
-            Rails.application.config.database_configuration[Rails.env]
-        config['pool'] = ENV['MAX_THREADS'] || 5
-        ActiveRecord::Base.establish_connection(config)
-      end
+  on_worker_boot do
+    # Valid on Rails up to 4.1 the initializer method of setting `pool` size
+    ActiveSupport.on_load(:active_record) do
+      config = ActiveRecord::Base.configurations[Rails.env] ||
+          Rails.application.config.database_configuration[Rails.env]
+      config['pool'] = ENV['MAX_THREADS'] || 5
+      ActiveRecord::Base.establish_connection(config)
     end
   end
 end
