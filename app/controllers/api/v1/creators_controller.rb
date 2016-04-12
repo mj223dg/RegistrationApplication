f71800764e0c281ae6f9e000c5195516fce872ef
mj223dg <mj223dg@student.lnu.se>
2016-04-12 19:49:24 +0200

    forgot present? in creatorcontroller, positioncontroller

--- app/controllers/api/v1/creators_controller.rb
+++ app/controllers/api/v1/creators_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::CreatorsController < Api::V1::ApiController
   respond_to :json
+
+  COULDNT_FIND_ID = "couldnt find an creator with the id of "
   def index
     respond_with Creator.limit(@limit).offset(@offset)
   end
 
   def show
+    creator = Creator.find_by_id(params[:id])
+    if creator.present?
     respond_with Creator.find_by_id(params[:id])
+    else
+      render json:  {error: COULDNT_FIND_ID {params[:id]}}, status: :not_found
+    end
   end
-
 end
--- app/controllers/api/v1/positions_controller.rb
+++ app/controllers/api/v1/positions_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::PositionsController < Api::V1::ApiController
   respond_to :json
+
+  COULDNT_FIND_ID = "couldnt find a position with the id of "
   def index
     respond_with Position.limit(@limit).offset(@offset)
   end
 
   def
     show
+    position = Position.find_by_id(params[:id])
+    if position.present?
     respond_with Position.find_by_id(params[:id])
+    else
+      render json: {error: COULDNT_FIND_ID {params[:id]}}, status: :not_found
+    end
   end
 end
